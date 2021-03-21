092202836453136b7f63ca3597e90e4eed3752be
ShivanshVerma-coder <75882684+ShivanshVerma-coder@users.noreply.github.com>
2021-03-21 18:08:18 +0530

    Update Javascript.js

--- Javascript.js
+++ Javascript.js
@@ -1,21 +1,23 @@
 /*-----------------BELOW IS PRELOADER-------------*/
 
 let timer1
-let timer2
-let timer3
 let preloader=document.querySelector('.preloader')
-/*timer1=setInterval(()=>{
-    preloader.classList.add('endpreloadynev');
-    setTimeout(timer1);
-},4500)*/
-/*timer2=setInterval(()=>{
-    preloader.classList.add('endpreloadypos');
-    setTimeout(timer2);
-},4700)*/
-timer3=setInterval(()=>{
+let nav=document.querySelector('.nav');
+timer1=setInterval(()=>{
 preloader.classList.add('endpreloadopacity');
-setTimeout(timer3);
+setTimeout(timer1);
 document.body.style.background="white";
 },4500)
+let timer2= setInterval(()=>{
+    nav.classList.remove('nav_beforepreload');
+    setTimeout(timer2);
+    document.body.style.transition="0s none";
+document.body.style.transition.delay="none";
+document.querySelector('.headervdo').classList.add('headervdo_afterpreload');
+},4600)
+let timer3= setInterval(()=>{
+    preloader.style.display='none';
+    setTimeout(timer3);
+},5500)
 
 /*-----------------ABOVE WAS PRELOADER-------------*/
