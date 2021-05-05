49c92ae9e9b81df4ffb339809108787faf377359
Shivansh Joshi <77457296+sniperzee2@users.noreply.github.com>
2021-05-05 22:50:04 +0530

    Update event_Javascript.js

--- event/event_Javascript.js
+++ event/event_Javascript.js
@@ -1,21 +1,21 @@
-let timer1
-let preloader=document.querySelector('.preloader')
-let nav=document.querySelector('.navbar');
-timer1=setInterval(()=>{
-preloader.classList.add('endpreloadopacity');
-setTimeout(timer1);
+// let timer1
+// let preloader=document.querySelector('.preloader')
+// let nav=document.querySelector('.navbar');
+// timer1=setInterval(()=>{
+// preloader.classList.add('endpreloadopacity');
+// setTimeout(timer1);
 
-},4500)
-let timer2= setInterval(()=>{
-    nav.classList.remove('nav_beforepreload');
-    setTimeout(timer2);
-    document.body.style.transition="0s none";
-document.body.style.transition.delay="none";
-},200)
-let timer3= setInterval(()=>{
-    preloader.style.display='none';
-    setTimeout(timer3);
-},5500)
+// },4500)
+// let timer2= setInterval(()=>{
+//     nav.classList.remove('nav_beforepreload');
+//     setTimeout(timer2);
+//     document.body.style.transition="0s none";
+// document.body.style.transition.delay="none";
+// },200)
+// let timer3= setInterval(()=>{
+//     preloader.style.display='none';
+//     setTimeout(timer3);
+// },5500)
 
 
 
